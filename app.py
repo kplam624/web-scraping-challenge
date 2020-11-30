6b722fb6584fbf6ddde5797af24619241c914ab3
kplam624 <kplam624@gmail.com>
2020-11-30 12:28:24 -0500

    Updated the app.py placed the index.html in a template folder.

--- app.py
+++ app.py
@@ -2,7 +2,26 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-
+#Create an instance of Flask
 app = Flask(__name__)
 
+#Connect to the mongodb database
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
+
+@app.route('/')
+    def home():
+        mars_data = mongo.db.collection.find_one()
+
+        return render_template("index.html", mars = mars_data)
+
+@app.route('/scrape')
+    def scrape():
+
+        mars_scrape = scrape_mars.scrape()
+
+        mongo.db.collection.update({}, mars_scrape, upsert = true)
+
+        return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
