1dbf23003da0ce6b5992fd44458a5d3f512a2d7d
kplam624 <kplam624@gmail.com>
2020-12-02 12:12:54 -0500

    Commented the python scripts.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Import Dependancies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
@@ -9,19 +10,24 @@ app = Flask(__name__)
 #Connect to the mongodb database
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
 
+# Create the home route
 @app.route('/')
 def home():
     mars_data = mongo.db.collection.find_one()
 
     return render_template("index.html", mars = mars_data)
 
+# Create the scrape route
 @app.route('/scrape')
 def scrape():
 
+# Start scraping
     mars_scrape = scrape_mars.scrape()
 
+# Updates the mongo database with a new collection and updates it.
     mongo.db.collection.update({}, mars_scrape, upsert = True)
 
+# Redirects the user to the home page.
     return redirect("/")
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -25,6 +25,7 @@ def scrape():
     html = browser.html
     soup = bs(html,'html')
 
+# Finds the article title and the description
     title = soup.find_all('div', class_ ="content_title")[1].text
     description = soup.find('div', class_ ="article_teaser_body").text
     #-------------------------------------------------------------------------------------------------
@@ -39,6 +40,8 @@ def scrape():
     html = browser.html
     soup = bs(html,'html')
 
+# Clicks around the webpage and will find the needed elements.
+# Finds the featured image url
     browser.find_by_tag('footer').click()
     time.sleep(2)
     browser.links.find_by_partial_text('more info').click()
@@ -50,12 +53,14 @@ def scrape():
     # Searches for the table.
     # Visit the webpage
 
+    # Find the table and moves it to an html string
     url3 = "https://space-facts.com/mars/"
     table = pd.read_html(url3)
     df = table[0]
     df.columns = ["","Mars"]
     df.set_index("", inplace = True)
 
+# html string format
     table = df.to_html()
 
     #-----------------------------------------------------------------------------------------------------
@@ -68,37 +73,56 @@ def scrape():
     html = browser.html
     soup = bs(html,'html')
 
+# Initializes the click process.
+# Finds all of the descriptions
     hemispheres = soup.find_all('div', class_ = 'description')
     hemilen = len(hemispheres)
+
+# Clicks on the first image
     browser.find_by_tag("h3").click()
+
+# Empty list to hold the images
     mars = []
 
+# For loop to go back and forth between the images.
     for x in range(1,hemilen+1):
+
+# Finds the url for the image.
         wide = browser.find_by_id("wide-image")
         img = wide.find_by_tag('img')
         image = img[1]['src']
-        
+
+# Appends the image to the list, sleep is for the back button to load
         mars.append(image)
         time.sleep(2)
+
+# Tells the clicker to go to the previous page.
         browser.back()
-        
+
+        # Attempts to click the next link
         try:
             browser.find_by_tag("h3")[x].click()
+
+        # When the clicking is done. Prints done.
         except:
             print('done')
 
+# Creates a list of dictionaries
+# Adds the list of images to the dictionaries
     mars_list = [
         {'title':'Cerberus Hemisphere', "image": mars[0]},
         {'title':'Schiaparelli Hemisphere', "image": mars[1]},
         {'title':'Syrtis Major Hemisphere', "image": mars[2]},
         {'title':'Valles Marineris Hemisphere Hemisphere', "image": mars[3]}
     ]
-    
+
+    # This list of dictionaries is what will be inputted into mongodb
     m_dict={
         'featured_url':featured_img_url,
         'feature_title':title,
         'feature_description':description,
         'table1': table,
+    # To pass the list of dictionaries, use a nested dictionary.
         'hemi1': mars_list[0],
         'hemi2': mars_list[1],
         'hemi3': mars_list[2],
